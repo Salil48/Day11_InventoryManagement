6464957941ffad1d41f91ca48cee0f662499f47a
“Salil48” <salilchharia@gmail.com>
2023-01-05 21:33:23 +0530

    Welcome to Inventory Management Program

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace InventoryManagement1
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            string path = "J:\\240\\InventoryManagement1\\InventoryManagement1\\Inventory.json";
+
+            ReadData readData = new ReadData();
+
+            var Data = readData.Read(path);
+
+            Console.WriteLine("Types of Rice");
+            Console.WriteLine("-------------------");
+            for (int i = 0; i < Data.typesOfRice.Count; i++)
+            {
+                Console.WriteLine(Data.typesOfRice[i].Name);
+                Console.WriteLine(Data.typesOfRice[i].Kg);
+                Console.WriteLine(Data.typesOfRice[i].Price);
+                Console.WriteLine(Data.typesOfRice[i].Type);
+                Console.WriteLine("-------------------");
+            }
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Types of Pulses");
+            Console.WriteLine("-----------------");
+            for (int i = 0; i < Data.typesOfPulses.Count; i++)
+            {
+                Console.WriteLine(Data.typesOfPulses[i].Name);
+                Console.WriteLine(Data.typesOfPulses[i].Kg);
+                Console.WriteLine(Data.typesOfPulses[i].Price);
+                Console.WriteLine(Data.typesOfPulses[i].Type);
+                Console.WriteLine("-------------------");
+            }
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Types of Wheat");
+            Console.WriteLine("-------------------");
+            for (int i = 0; i < Data.typesOfWheat.Count; i++)
+            {
+                Console.WriteLine(Data.typesOfWheat[i].Name);
+                Console.WriteLine(Data.typesOfWheat[i].Kg);
+                Console.WriteLine(Data.typesOfWheat[i].Price);
+                Console.WriteLine(Data.typesOfWheat[i].Type);
+                Console.WriteLine("-------------------");
+            }
+        }
+    }
+}
