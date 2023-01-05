d7357f80727be1f90bdcdf7622b41ca9c9159f45
“Salil48” <salilchharia@gmail.com>
2023-01-05 21:38:06 +0530

    Welcome to Inventory Management Program

---
+++ InventoryDetails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement1
+{
+    public class InventoryDetails
+    {
+        public List<TypesOfRice> typesOfRice;
+        public List<TypesOfPulses> typesOfPulses;
+        public List<TypesOfWheat> typesOfWheat;
+    }
+
+    public class TypesOfRice
+    {
+        public string Name;
+        public int Kg;
+        public int Price;
+        public string Type;
+    }
+    public class TypesOfPulses
+    {
+        public string Name;
+        public int Kg;
+        public int Price;
+        public string Type;
+    }
+    public class TypesOfWheat
+    {
+        public string Name;
+        public int Kg;
+        public int Price;
+        public string Type;
+    }
+}
